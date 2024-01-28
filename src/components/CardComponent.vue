5c1414581545b4caee8938a9c1f47e36ab04705c
MY <x5426854268@gmail.com>
2024-01-28 22:12:59 +0800

    git add

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        這是一個卡片元件<br>
+        {{text}}
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            text:'文字'
+        }
+    }
+}
+</script>
